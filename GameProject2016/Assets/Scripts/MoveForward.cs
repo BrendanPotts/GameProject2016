f1dbbb1b8b9303ca1c06ae42bece5c71c26a484f
BrendanPotts <pottsbrendan@rocketmail.com>
2016-10-05 16:16:36 +0100

    Revert "clear for merge"
    This reverts commit 792634bc7b925adda88a0bba8242de9131e425a0.

---
+++ GameProject2016/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+
+	public static float speed;
+	public float cruisespeed = 30.0f;
+
+	void Start(){
+		speed = 1;
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (speed < cruisespeed) {
+			speed  += 0.1f;
+		}
+		transform.position += transform.forward*speed*Time.deltaTime;
+		if (speed >= 50) {
+			print (speed);
+		}
+	}
+}
